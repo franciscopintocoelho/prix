37370fb5a550aa2c2d753142ef591131a7ef4ed8
Francisco Coelho <francisco.coelho@fullsix.local>
2016-08-24 19:14:05 +0100

    added sensor data

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # prix
+
+Requirements:
+
+1. Install Jessie Lite
+2. Update and Upgrade
+3. Expand Filesystem
+4. Remove Login
+5. Install NodeJS
+6. Enable I2C Bus
+7. Install Git
+8. Clone Repository
+9. Install npm packages
+10. Install omxplayer (?)
+10. Setup Autorun
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var child = require('child_process');
 var app = express();
 
 var iothub = require('azure-iothub');
+var i2c = require('ic2-bus');
 
 var deviceId = 'myFirstNodeDevice';
 var connectionString = 'HostName=PRIX.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=QYtJqH9+59PQ5IdOszsCzR30kVJk1302zl5gEzz3pA0=';
@@ -14,8 +15,8 @@ var Message = require('azure-iot-device').Message;
 
 var device = new iothub.Device(null);
 
-var PythonShell = require('python-shell');
-var pyshell;
+var bus = i2c.openSync(1);
+var dist, address = 0x70;
 
 device.deviceId = deviceId;
 registry.create(device, function (err, deviceInfo, res) {
@@ -28,16 +29,35 @@ registry.create(device, function (err, deviceInfo, res) {
 });
 
 function printResultFor(op) {
-  return function printResult(err, res) {
-    if (err) console.log(op + ' error: ' + err.toString());
-    if (res) console.log(op + ' status: ' + res.constructor.name);
-  };
+    return function printResult(err, res) {
+        if (err) console.log(op + ' error: ' + err.toString());
+        if (res) console.log(op + ' status: ' + res.constructor.name);
+    };
+}
+
+function getDistance() {
+    bus.writeByte(address, 0, 81, function () {
+        setTimeout(function () {
+            bus.readWord(address, 2, function (err, data) {
+                if (!err) 
+                getDistance();
+            });
+        }, 50);
+    });
+};
+
+function sendData(data) {
+    console.log(data);
+    //var data = JSON.stringify({ deviceId: deviceId, distance: message });
+    //var message = new Message(data);
+    //console.log("Sending message: " + message.getData());
+    //client.sendEvent(message, printResultFor('send'));
 }
 
 function getDeviceInfo(err, deviceInfo, res) {
     if (deviceInfo) {
         var deviceString = 'HostName=PRIX.azure-devices.net;DeviceId=' + deviceInfo.deviceId + ';SharedAccessKey=' + deviceInfo.authentication.SymmetricKey.primaryKey;
-        
+
         console.log('Device id: ' + deviceInfo.deviceId);
         console.log('Device key: ' + deviceInfo.authentication.SymmetricKey.primaryKey);
 
@@ -49,16 +69,7 @@ function getDeviceInfo(err, deviceInfo, res) {
             } else {
                 console.log('Client connected');
                 // Create a message and send it to the IoT Hub every second
-
-                pyshell = new PythonShell('sensor.py');
-                
-                pyshell.on('message', function(message) {
-                    console.log(message);
-                    //var data = JSON.stringify({ deviceId: deviceId, distance: message });
-                    //var message = new Message(data);
-                    //console.log("Sending message: " + message.getData());
-                    //client.sendEvent(message, printResultFor('send'));
-                });
+                getDistance();
             }
         };
 
@@ -68,27 +79,27 @@ function getDeviceInfo(err, deviceInfo, res) {
 
 app.set('port', (process.env.PORT || 8080));
 
-app.get('/update/', function(req, res) {
-    child.exec('git pull', function(error, stdout, stderr) {
-        if(!error) {
+app.get('/update/', function (req, res) {
+    child.exec('git pull', function (error, stdout, stderr) {
+        if (!error) {
             console.log(stdout);
-            child.exec('npm install', function(error, stdout, stderr) {
-                if(!error) {
+            child.exec('npm install', function (error, stdout, stderr) {
+                if (!error) {
                     res.status(200).send('updated');
                     console.log(stdout);
-                    child.exec('sudo reboot', function(error, stdout, stderr) {
-                        if(!error) {
+                    child.exec('sudo reboot', function (error, stdout, stderr) {
+                        if (!error) {
                             console.log(stdout);
                             process.exit()
                         } else console.log(stderr);
                     });
-                    process.exit()
+                    process.exit();
                 } else console.log(stderr);
             });
         } else console.log(stderr);
     });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
