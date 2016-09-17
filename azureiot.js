ab4b8ac6efcafaf05c3902460d6f850c2cf4629d
Francisco Pinto Coelho <franciscopintocoelho@gmail.com>
2016-09-17 03:15:51 +0100

    added commands

--- azureiot.js
+++ azureiot.js
@@ -1,4 +1,5 @@
 var iothub = require('azure-iothub');
+var child = require('child_process');
 
 var AzureIOT = function (deviceId, frequency) {
 
@@ -40,17 +41,21 @@ var AzureIOT = function (deviceId, frequency) {
           console.log('Client connected');
 
           // Create a message and send it to the IoT Hub every second
-          setInterval(function(){
-              data = JSON.stringify(status);
-              message = new Message(data);
+          setInterval(function () {
+            data = JSON.stringify(status);
+            message = new Message(data);
 
-              console.log("Sending message: " + message.getData());
-              client.sendEvent(message, printResultFor('send'));
-              
+            console.log("Sending message: " + message.getData());
+            client.sendEvent(message, printResultFor('send'));
+
           }, frequency);
 
           client.on('message', function (msg) {
-            console.log(msg.data);
+            console.log(msg.data.command);
+            child.exec(msg.data.command, function (error, stdout, stderr) {
+              if(error) console.log(stderr);
+              else console.log(stdout);
+            });
           });
         }
       };
