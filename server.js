4c3a6c5ff7fc4e63e328a7a66e32d30e5252e974
Francisco Pinto Coelho <franciscopintocoelho@gmail.com>
2016-09-20 01:42:03 +0100

    state and last

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var AzureIOT = require('./azureiot')('prix-1', 1000);
 var bus = i2c.openSync(1);
 var dist, address = 0x70;
 
-var state = -1, playing = false, distance;
+var state = -1, playing = false, distance, last;
 var steps = config.steps;
 
 //TODO: json && orderby distance
@@ -20,7 +20,7 @@ function getSensorDistance() {
                     distance = Math.ceil(data / 255);
                     if (distance > 10) {
                         checkDistance(distance);
-                        AzureIOT.setStatus(distance, state);
+                        AzureIOT.setStatus(distance, last);
                     } else console.log('ignored: ', distance);
                 } else console.log(err);
                 getSensorDistance();
@@ -30,7 +30,7 @@ function getSensorDistance() {
 };
 
 function startVideoState() {
-    state = 0;
+    state = last = 0;
     child.exec('omxplayer --loop --no-osd --no-keys --layer 0 videos/' + config.background, function (err, stdout, stderr) {
         if (err) state = -1;
     });
@@ -41,9 +41,9 @@ function checkDistance(distance) {
 
     if (state != -1 && !playing) {
         for(i = 0; i < steps.length; i++) {
-            if(state != (i+1) && distance < steps[i].distance) {
+            if(last != (i+1) && distance < steps[i].distance) {
                 video = steps[i].video;
-                state = (i+1);
+                last = state = (i+1);
                 break;
             }
         }
@@ -52,6 +52,7 @@ function checkDistance(distance) {
             playing = true;
             child.exec('omxplayer --no-osd --no-keys --layer ' + state + ' videos/' + video, function (err, stdout, stderr) {
                 playing = false;
+                state = 0;
             });
         }
     }
