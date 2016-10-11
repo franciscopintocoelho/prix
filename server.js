9578e254d91a9894e4740debc647988503e4f2b7
Francisco Pinto Coelho <franciscopintocoelho@gmail.com>
2016-10-11 19:26:55 +0100

    omx manager added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var child = require('child_process');
+var omxplayer = require('omx-player');
 var i2c = require('i2c-bus');
 
 var config = require('./config.json');
@@ -10,11 +11,12 @@ var dist, address = 0x70;
 var state = -1, playing = false, distance, interval;
 var steps = config.steps;
 
-//TODO: json order by distance
+var manager = new omxplayer();
+var background, videos;
 
 function getSensorDistance() {
     bus.writeByte(address, 0, 81, function (err) {
-        if(!err) {
+        if (!err) {
             setTimeout(function () {
                 bus.readWord(address, 2, function (err, data) {
                     if (!err) {
@@ -37,38 +39,89 @@ function getSensorDistance() {
     });
 };
 
+function getSimulatedSensorDistance() {
+    var stdin = process.stdin;
+
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf-8');
+    stdin.resume();
+
+    stdin.on('data', function (key) {
+        switch (key) {
+            case '\u0003':
+                process.exit();
+                break;
+            case '\u001B\u005B\u0041':
+                distance+= 20;
+                break;
+            case '\u001B\u005B\u0042':
+                distance-= 20;
+                break;
+        }
+    });
+
+    distance = 130;
+
+    setInterval(function () {
+        AzureIOT.setStatus(distance, state);
+        if (distance > 10) {
+            checkDistance(distance);
+        } else console.log('ignored: ', distance);
+    }, 50);
+};
+
 function startVideoState() {
     state = 0;
-    child.exec('omxplayer --loop --no-osd --no-keys --layer 0 videos/' + config.background, function (err, stdout, stderr) {
+    videos = [];
+    background = manager.create(config.background);
+    
+    for(var i = 0; i < config.steps.length; i++) {
+        videos.push(manager.create(config.steps[i].video));
+    }
+
+    /*child.exec('omxplayer --loop --no-osd --no-keys --layer 0 videos/' + config.background, function (err, stdout, stderr) {
         if (err) {
             AzureIOT.sendError(err);
             state = -1;
         }
-    });
+    });*/
+
+    background.play();
 }
 
 function checkDistance(distance) {
     var video, i;
 
     if (state != -1 && !playing) {
-        for(i = 0; i < steps.length; i++) {
-            if(distance < steps[i].distance && state != (i+1)) {
-                video = steps[i].video;
-                state = (i+1);
+        for (i = 0; i < steps.length; i++) {
+            if (distance < steps[i].distance && state != (i + 1)) {
+                //video = steps[i].video;
+                video = videos[i];
+                state = (i + 1);
                 break;
             }
         }
 
         if (video) {
             playing = true;
-            child.exec('omxplayer --no-osd --no-keys -o both --layer ' + state + ' videos/' + video, function (err, stdout, stderr) {
-                if(err) AzureIOT.sendError(err);
+            /*child.exec('omxplayer --no-osd --no-keys -o both --layer ' + state + ' videos/' + video, function (err, stdout, stderr) {
+                if (err) AzureIOT.sendError(err);
 
                 state = 0;
-                setTimeout(function() {
+                setTimeout(function () {
+                    playing = false;
+                }, 3000);
+            });*/
+
+            video.on('end', function() {
+                state = 0;
+                setTimeout(function () {
                     playing = false;
                 }, 3000);
             });
+
+            video.play();
+            
         }
     }
 };
